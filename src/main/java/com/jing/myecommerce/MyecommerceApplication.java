a55bbfe08123ec3c6dcd10b3d8b7c4f1e4e1512c
Jing <zhoujing6371@gmail.com>
2020-12-26 13:41:40 -0600

    start

---
+++ src/main/java/com/jing/myecommerce/MyecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.jing.myecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyecommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jing/myecommerce/MyecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jing.myecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyecommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
